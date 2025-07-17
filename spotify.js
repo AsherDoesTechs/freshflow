945c952cc99d278c6b102ce0e0a7c5e537c5c3a1
AsherDoesTechs <Asherbasco93@gmail.com>
2025-07-17 23:01:13 +0800

    Added the API Get a Song Recomendation bellow the client credentials

--- spotify.js
+++ spotify.js
@@ -16,3 +16,25 @@ export async function getAccessToken() {
   const data = await res.json();
   return data.access_token;
 }
+
+export async function getRandomTrack(token) {
+  const res = await fetch(
+    `https://api.spotify.com/v1/recommendations?seed_genres=lofi&limit=1`,
+    {
+      method: `GET`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const data = await res.json();
+  const track = data.tracks[0];
+
+  return {
+    name: track.name,
+    artist: track.artist[0].name,
+    image: track.album.images[0].url,
+    url: track.external_urls.spotify,
+  };
+}
